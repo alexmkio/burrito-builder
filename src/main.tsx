ba2d6b396216270692fc4e073e190e03cb607e04
Alex Kio <alexandermartelkio@gmail.com>
2023-01-08 17:43:42 -0500

    Add root route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.scss";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import FormAndOrders from "./components/FormAndOrders/FormAndOrders";
+import Orders from "./components/Orders/Orders";
+import Burrito, { loader as burritoLoader } from "./components/Burrito/Burrito";
+      {
+        path: "/",
+        element: <FormAndOrders />,
+      },
+      {
+        path: "orders/",
+        element: <Orders />,
+      },
+      {
+        path: "order/:orderId",
+        element: <Burrito />,
+        loader: burritoLoader,
+      },
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
