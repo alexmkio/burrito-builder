703e9fa19ae5a221dd953ec74629f1448105b2f7
Alex Kio <alexandermartelkio@gmail.com>
2023-01-08 18:24:04 -0500

    Use location to conditionally render nav links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,27 @@
 import "./Header.scss";
 import reactLogo from "../../assets/react.svg";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Header() {
+  let path = useLocation().pathname;
+
   return (
     <header>
       <img src={reactLogo} className="logo" alt="React logo" />
       <h1>Burrito Builder</h1>
+      <nav>
+        {path === "/" ? (
+          <Link to="orders/">Orders</Link>
+        ) : path === "/orders/" ? (
+          <Link to="/">Home</Link>
+        ) : (
+          <>
+            <Link to="/">Home</Link>
+            <Link to="orders/">Orders</Link>
+          </>
+        )}
+      </nav>
     </header>
   );
 }
